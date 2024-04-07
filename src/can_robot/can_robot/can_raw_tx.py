6dfcf65b2cd43540bdfad7c2c368b46b31ade320
LiamKaist <liamchrisment@gmail.com>
2024-04-07 20:48:22 +0000

    Working can_raw_tx, some improvements needed, report will be written

--- src/can_robot/can_robot/can_raw_tx.py
+++ src/can_robot/can_robot/can_raw_tx.py
@@ -5,53 +5,54 @@ import rclpy
 from rclpy.node import Node
 import can
 
-from can_raw_interfaces.msg import CanRaw 
+from can_raw_interfaces.msg import CanRaw
 
 class CanRawTx(Node):
-    
+
     def __init__(self):
         """ CanRawTx constructor """
         super().__init__('can_raw_tx') #Call to Node constructor with a parameter
-        
+
         # Sets up a link to a linux socketcan CAN bus interface
-        self.can_bus = can.Bus(interface='socketcan',
+        self.can_bus = can.ThreadSafeBus(interface='socketcan',
                                      channel='can0',
                                      bitrate=125000,
-                                     receive_own_messages=True)
-        
-        self.notifier = can.Notifier(self.can_bus, [self.on_message])
-        
+                                     receive_own_messages=False)
+
+        # Sets up a bus for listening to the CAN bus
+        self.can_bus_notifier = can.ThreadSafeBus(interface='socketcan',
+                                     channel='can0',
+                                     bitrate=125000,
+                                     receive_own_messages=False)
+
+        self.notifier = can.Notifier(self.can_bus_notifier, [self.on_message])
+
         self.can_raw_tx_subscriber = self.create_subscription(CanRaw, 'can_raw_tx',self.on_topic_data, 10) #Subscription to can_raw_tx with CanRaw interface definition
-    
+
     def on_topic_data(self,can_raw_tx_msg):
         """ Callback which is called whenever raw transmission data arrives in the can_raw_tx topic """
-        
+
         self.send_single_msg(can_raw_tx_msg.arbitration_id,can_raw_tx_msg.data,can_raw_tx_msg.err_flag,can_raw_tx_msg.rtr_flag,can_raw_tx_msg.eff_flag)
-        self.get_logger().info("Who summoned me?")
-    
+
     def on_message(self, msg):
         """
         Callback which is called whenever the CAN interface detects incoming traffic on the BUS
         """
         self.get_logger().info('Received message: {0}'.format(msg))
-        
+
     def send_single_msg(self,arbitration_id,data,err_flag,rtr_flag,eff_flag):
         """ Sends a single message on the CAN bus """
-        
-        with can.Bus() as bus:
-            
-            msg = can.Message(
-                arbitration_id=arbitration_id, data=data, is_extended_id=False
-            )
-            
-            try:
-                bus.send(msg)
-                print(f"Message sent on {bus.channel_info}")
-                
-            except can.CanError:
-                print("Message NOT sent")
-        
-        
+
+        msg = can.Message(
+            arbitration_id=arbitration_id, data=data, is_extended_id=False
+        )
+        try:
+            self.can_bus.send(msg)
+            print(f"Message sent on {self.can_bus.channel_info}")
+
+        except can.CanError:
+            print("Message NOT sent")
+
 def main(args=None):
     rclpy.init(args=args) #Initialise ROS2 communications
 
