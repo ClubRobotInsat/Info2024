78f5e285411048f6763552d9b73bf3b6f5e3df07
Ronan <ronan.bonnet42@wanadoo.fr>
2024-04-26 15:31:59 +0200

    Make controller for the driving base.

---
+++ src/my_robot_control/scripts/control_can.py
@@ -0,0 +1,79 @@
+#!/usr/bin/python3
+
+import rclpy
+from rclpy.node import Node
+import math
+from std_msgs.msg import Float64MultiArray
+
+import threading
+import numpy as np
+
+from can_interface.msg import MotorCmd
+
+# Create dictionary to store id of each motor
+motor_id = {
+    'left': 0,
+    'front': 1,
+    'right': 2,
+    'back': 3
+}
+
+class FVCSubscriberCan(Node):
+    def __init__(self):
+        super().__init__('fvc_subscriber_can')
+        self.subscriber = self.create_subscription(Float64MultiArray, '/forward_velocity_controller/commands', self.listener_callback, 10)
+        self.publisher = self.create_publisher(MotorCmd, '/motor_cmd', 10)
+    def listener_callback(self, data):
+
+        # self.get_logger().info('I heard: "%s"' % data.data)
+
+        # For each motor, we have to make a motorMsg and publish it
+        # LIAAAAAAAAM
+        for i in range(4):
+            # Get the motor id from the dictionary
+            motor = list(motor_id.keys())[list(motor_id.values()).index(i)]
+
+            motorMsg = MotorCmd()
+            if motor == 'left' or motor == 'right':
+                motorMsg.dest = 2
+                if motor == 'left':
+                    motorMsg.motor_id = 0
+                if motor == 'right':
+                    motorMsg.motor_id = 1
+            else:
+                motorMsg.dest = 3
+                if motor == 'front':
+                    motorMsg.motor_id = 0
+                if motor == 'back':
+                    motorMsg.motor_id = 1
+            motorMsg.command_id = 3 # Set speed
+
+            if data.data[i] > 0:
+                motorMsg.direction = 1
+            else:
+                motorMsg.direction = 0
+            # 0.9 m/s is the max speed of the robot
+            motorMsg.speed = data.data[i] * 1000.0 # Convert from m/s to mm/s
+            motorMsg.extra = 0
+            self.publisher.publish(motorMsg)
+
+if __name__ == '__main__':
+    rclpy.init(args=None)
+
+    fvc_subscribercan = FVCSubscriberCan()
+
+    executor = rclpy.executors.MultiThreadedExecutor()
+    executor.add_node(fvc_subscribercan)
+
+    executor_thread = threading.Thread(target=executor.spin, daemon=True)
+    executor_thread.start()
+    rate = fvc_subscribercan.create_rate(2)
+    try:
+        while rclpy.ok():
+            rate.sleep()
+    except KeyboardInterrupt:
+        pass
+
+    rclpy.shutdown()
+    executor_thread.join()
+
